c6b254e4e1e40804d5690b693ea3f210348f9632
Wacharin Naktubtim <wacharin_loveled@hotmail.com>
2015-07-15 12:05:48 +0700

    แก้ไขเพิ่มเติม  controllers

--- work1/application/controllers/login.php
+++ work1/application/controllers/login.php
@@ -6,26 +6,42 @@ class Login extends CI_Controller {
 	
 	public function index()
 	{
-		if($this->input->post())
-		{
-			$this->load->model('User');
-			$this->User->setUsername($this->input->post('username'));
-			$this->User->setPassword($this->input->post('password'));
+		$this->load->helper('url');
+
+	if(isset($_COOKIE['username']) && isset($_COOKIE['password']))
+	{
+		$this->load->model('User');
+		$this->User->setUsername($_COOKIE['username']);
+		$this->User->setPassword($_COOKIE['password']);
 		
-		if($this->User->CheckLogin())
-		{
-			//ถ้า login ผ่าน
-			echo 'Pass';
-		}
-		else
-		{
-			//ถ้า login ไม่ผ่าน
-			echo 'Fail';
-		}
+			if($this->User->CheckLogin())
+			{
+				redirect(base_url('index.php/memberzone')); //ต้องมี  helper เท่านั้น
+			}
 	}
 
+	if($this->input->post())
+	{
+		$this->load->model('User');
+		$this->User->setUsername($this->input->post('username'));
+		$this->User->setPassword($this->input->post('password'));
+		
+			if($this->User->CheckLogin())
+			{
+				//ถ้า login ผ่าน
+				echo 'Pass';
+				setcookie('username', $this->input->post('username'));
+				setcookie('password', $this->input->post('password'));
+				redirect(base_url('index.php/memberzone')); //ต้องมี  helper เท่านั้น
+			}
 
-		$this->load->helper('url');
+			else
+			{
+				//ถ้า login ไม่ผ่าน
+				echo 'Fail';
+			}
+	}
+
 		$this->load->view('login');
 	}
 }
